b47987842b693dbe0101ee0eaf5f759a78200aa2
El-Ingeniero79 <150141876+El-Ingeniero79@users.noreply.github.com>
2024-03-18 13:16:07 +0100

    Update checkpoint5.py

--- checkpoint5.py
+++ checkpoint5.py
@@ -14,8 +14,17 @@ suma (1, 2, 3)
 
 sum = lambda a, b, c:a + b + c
 print (sum(1, 2, 3))
+
 # determinar si un valor coincide o no con un valor de la lista
 
 lista_nombre = ["Jessica", "Paul", "George", "Henry", "Adam"]
 nombre = "Enrique"
-print (nombre in lista_nombre)
+encontrado = False
+for nom in lista_nombre:
+    if nom == nombre:
+        encontrado = True
+        break
+if encontrado:
+    print(f"{nombre} esta en la lista")
+else:
+    print(f"{nombre} NO esta en la lista")
